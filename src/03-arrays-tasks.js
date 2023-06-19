004ef131ecdc1a0b19282477ddf19e559859f94c
Nickoff <v3136820@gmail.com>
2023-06-19 10:26:37 +0300

    feat: add solution group and distinct to 03-array

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -483,8 +483,8 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return [...new Set(arr)];
 }
 
 /**
@@ -517,8 +517,18 @@ function distinct(/* arr */) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  return array.reduce((map, item) => {
+    const key = keySelector(item);
+    const value = valueSelector(item);
+    const collection = map.get(key);
+    if (!collection) {
+      map.set(key, [value]);
+    } else {
+      collection.push(value);
+    }
+    return map;
+  }, new Map());
 }
 
 /**
